6fc0960c1cf1b2baeec1fa92f7a02ae105cdbac2
Rashaun Bennett <99235834+bird556@users.noreply.github.com>
2022-08-17 14:59:24 -0400

    Update weatherAPI.js

--- weatherAPI.js
+++ weatherAPI.js
@@ -3,7 +3,7 @@
 function fetchWeatherData() {
   //fetch data and dynamically add the city name with template literals
   fetch(
-    `https://api.weatherapi.com/v1/current.json?key=1bc32d2dbafb49afba9182232221708=${cityInput}`
+    `https://api.weatherapi.com/v1/current.json?key=1bc32d2dbafb49afba9182232221708&q=${cityInput}`
   )
     // Now take data (JSON Format) and convert it to a regular JS OBJECT
     .then((response) => response.json())
